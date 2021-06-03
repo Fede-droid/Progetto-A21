22048ac8af766803eddd5eede73cac31cc2f6279
fabiotagliani <fabiotagliani@mbp-di-fabio.fritz.box>
2021-06-04 00:25:48 +0200

    update

--- src/Model/Core/Multiplayer/Client.java
+++ src/Model/Core/Multiplayer/Client.java
@@ -41,7 +41,7 @@ public class Client {
 	            
 	            
 	            boolean waitingForReply = true;
-	           
+	           //
 	            while (waitingForReply) {
 	           
 	            	
