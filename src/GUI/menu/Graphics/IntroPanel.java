d602061b4e92b89a9691c981928ce85cd9f43dc1
fabiotagliani <fabiotagliani@mbp-di-fabio.fritz.box>
2021-07-21 10:16:10 +0200

    risolto un piccolo bug sulla intro

--- src/GUI/menu/Graphics/IntroPanel.java
+++ src/GUI/menu/Graphics/IntroPanel.java
@@ -3,8 +3,13 @@ package GUI.menu.Graphics;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -17,7 +22,7 @@ import Model.Items.Utilities;
 public class IntroPanel extends JPanel {
 
 	
-	public IntroPanel() {
+	public IntroPanel() throws InterruptedException {
 		
 		ImagesLoader loader = new ImagesLoader();
  
@@ -26,13 +31,23 @@ public class IntroPanel extends JPanel {
 
 		Icon imgIcon = new ImageIcon(this.getClass().getResource("intro-2.gif"));
 		JLabel label = new JLabel(imgIcon);
-
 		label.setSize(Utilities.SCREEN_WIDTH, Utilities.SCREEN_HEIGHT);
 		
 		label.setBounds(0, 0, Utilities.SCREEN_WIDTH, Utilities.SCREEN_HEIGHT);
 		add(label);
 		setBackground(Color.BLACK);
 
+		String musicString = "./src/GUI/menu/menuImages/intro.wav";
+		try {
+		    AudioInputStream audio = AudioSystem.getAudioInputStream(new File(musicString).getAbsoluteFile());
+	        Clip hit = AudioSystem.getClip();
+	        hit.open(audio);
+	        hit.start();
+	        } catch(Exception ex) {
+	        System.out.println("Error with playing sound.");
+	        ex.printStackTrace();
+	    }
+	
         setVisible(true);
 		
 	}
